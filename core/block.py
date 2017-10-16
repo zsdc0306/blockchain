1d9ccc141cf98e7d6370b62c4c3e9732f21e3fc3
zsdc0306 <zsdc0306@gmail.com>
2017-10-16 02:12:07 -0400

    generate new block finished get blockchian finished block object finished

--- core/block.py
+++ core/block.py
@@ -1,34 +1,49 @@
 from time import time
 from hashlib import sha256
-from operation import calculate_hash_for_block
+import csv
 
-blockchain_file_name = 'blockchain'
+blockchain_file_name = '/Users/zsdc0306/Documents/project/blockchain/blockchain.csv'
+
 
 class Block(object):
-    def __init__(self, index=None, pre_hash=None, data=None, hash_val=None, time_stamp=str(time()).split('.')[0]):
+    def __init__(self):
+        self.index = None
+        self.pre_hash = None
+        self.time_stamp = None
+        self.data = None
+        self.hash_val = None
+
+    def set_block(self, index, pre_hash, data, time_stamp=None, hash_val=None):
         self.index = index
         self.pre_hash = str(pre_hash)
-        self.time_stamp = time_stamp
         self.data = data
-        self.hash_val = str(hash_val)
+        self.time_stamp = str(time()).split('.')[0] if time_stamp is None else time_stamp
+        self.hash_val = self.calculate_hash_for_block() if hash_val is None else str(hash_val)
+
 
     def init_first_block(self):
         self.index = 0
         self.pre_hash = "0"
         self.data = "This is the first block"
+        self.time_stamp = str(time()).split('.')[0]
         self.hash_val = self.calculate_hash_for_block()
 
     def store_block(self):
         block_str = self.stringfy_block()
-        with open(blockchain_file_name, 'a') as f:
-            f.write(block_str)
+        print block_str
+        try:
+            with open(blockchain_file_name, 'a') as f:
+                w = csv.writer(f)
+                w.writerow([str(self.index), self.pre_hash, str(self.time_stamp), self.data, self.hash_val])
+        except Exception as e:
+            print e.message
 
     def stringfy_block(self):
         content = [str(self.index), self.pre_hash, str(self.time_stamp), self.data, self.hash_val]
         return ','.join(content) + '\n'
 
     def calculate_hash_for_block(self):
-        return calculate_hash_for_block(self)
+        return str(sha256(str(self.index) + self.pre_hash + self.time_stamp + self.data).hexdigest())
 
     def validate_block(self, pre_block):
         return False if pre_block.index + 1 != self.index or pre_block.hash != self.pre_hash or \
@@ -37,20 +52,22 @@ class Block(object):
 
 class BlockChain(object):
     def __init__(self, blockchain_str=None):
-        self.block_chain = self.__get_existing_blockchain()
-        self.is_exist = True if len(self.block_chain) != 0 else False
-        self.lastest_block = self.block_chain[-1] if len(self.block_chain) != 0 else None
+        self.block_chain = [] if blockchain_str is None else self.__objectify_block(blockchain_str)
 
-    def load_existing_blockchian(self):
+    def get_latest_block(self):
+        return self.block_chain[-1] if len(self.block_chain) != 0 else None
+
+    def load_existing_blockchain(self):
         self.block_chain = self.__get_existing_blockchain()
 
     def __get_existing_blockchain(self):
         try:
             blockchain = []
-            with open(blockchain_file_name, 'r') as f:
-                content = f.readlines()
-                for line in content:
-                    blockchain += self.__load_block(line),
+            with open(blockchain_file_name, 'r+') as f:
+                reader = csv.reader(f)
+                print reader
+                for line in reader:
+                    blockchain += self.__objectify_block(line),
                 return blockchain
         except OSError as e:
             print e.message
@@ -59,8 +76,9 @@ class BlockChain(object):
             print e.message
             return []
 
-    def __load_block(self, block_str):
+    def __objectify_block(self, block_str):
         block_item = Block()
         block_item.index, block_item.pre_hash, block_item.data, block_item.hash_val, block_item.time_stamp = \
-            block_str.split(',')
+            block_str[:-1].split(',')
+        print block_item
         return block_item
--- core/operation.py
+++ core/operation.py
@@ -1,46 +1,72 @@
-from hashlib import sha256
 import block
+
+def init_block():
+    b = block.Block()
+    b.init_first_block()
+    b.store_block()
+
+
+def init_app():
+    global latest_block
+    latest_block = get_latest_block()
+    if latest_block is None:
+        print "init the first block"
+        init_block()
+        print "init app again"
+        init_app()
+    return True
+
+
+def ojbectfy_block(str):
+    block_item = str.split(",")
+    [index, pre_hash, time_stamp, data, hash_val] = block_item
+    b = block.Block()
+    b.set_block(index,pre_hash,data, time_stamp,hash_val)
+    return b
 
 
 def generate_block(data):
-    b = get_latest_block()
-    new_block = block.Block(int(b.index)+1, b.hash_val, data)
+    new_block = block.Block()
+    new_block.set_block(int(latest_block.index)+1, latest_block.hash_val, data)
     new_block.store_block()
-    return
-
-
-def calculate_hash_for_block(block):
-    return str(sha256(str(block.index) + block.pre_hash + block.time_stamp + block.data).hexdigest())
+    global latest_block
+    latest_block = new_block
+    return new_block
 
 
 def get_latest_block():
-    blockchain = block.BlockChain().load_existing_blockchian()  # get the bloch chain. It is a list of block object
-    return blockchain.block_chain[-1] if blockchain.is_exist else None
+    try:
+        with open(block.blockchain_file_name,"r") as f:
+            content = f.readlines()
+            if len(content) >= 1:
+                latest_block = content[-1]
+                latest_block = ojbectfy_block(latest_block)
+            else:
+                print "getting blockchain error"
+                return None
+        return latest_block
+    except Exception as e:
+        print e.message
+        return None
 
 
 def get_block_chain():
-    blockchain = block.BlockChain()
-    blockchain.load_existing_blockchian()
-    return blockchain.block_chain
+    with open(block.blockchain_file_name, 'r+') as f:
+        return f.readlines()
 
 
 def receive_block_chain(blockchain_str):
     block_chain = block.BlockChain()
-    block_chain.load_existing_blockchian()
-    block_obj = ojbectfy_blockchain(blockchain_str)
-    lastest_block = block_chain.lastest_block
-    if block_obj.validate_block(lastest_block):
+    block_chain.load_existing_blockchain()
+    block_obj = ojbectfy_block(blockchain_str)
+    latest_block = block_chain.get_latest_block()
+    if block_obj.validate_block(latest_block):
         block_obj.store_block()
         return True
     else:
         return False
 
 
-def ojbectfy_blockchain(str):
-    block_item = str.split(",")
-    [index, pre_hash, time_stamp, data, hash_val] = block_item
-    b = block.Block(index,pre_hash,data, hash_val, time_stamp)
-    return b
 
 
 
