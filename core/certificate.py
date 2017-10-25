9271b80e6a1c255ed3ce8c1b296e2fc319179b24
zsdc0306 <zsdc0306@gmail.com>
2017-10-25 00:27:40 -0400

    refactor the name of certificate build the structure for certificate

---
+++ core/certificate.py
@@ -0,0 +1,21 @@
+class Certificate(object):
+    def __init__(self):
+        # attribution for cert
+        pass
+
+    def set_cert(self):
+        pass
+
+    def create(self):
+        pass
+
+    def revoke(self):
+        pass
+
+    # convert the certificate to string
+    def serialize_cert(self):
+        pass
+
+    def deserialize_cert(self, cert_str):
+        pass
+
