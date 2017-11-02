072ec6b96144a807d38615a12d812afcb644d9e5
xulin2016 <xulin1874@gmail.com>
2017-11-01 22:37:16 -0400

    generate certificate

---
+++ core/generateCer.py
@@ -0,0 +1,28 @@
+from OpenSSL import crypto, SSL
+import uuid
+
+CERT_FILE = "selfsigned.crt"
+KEY_FILE = "private.key"
+
+def create_self_signed_cert():
+
+    # create a key pair
+    k = crypto.PKey()
+    k.generate_key(crypto.TYPE_RSA, 1024)
+    # create a self-signed cert
+    cert = crypto.X509()
+    # use mac address as cert identity
+    cert.get_subject().CN=':'.join(['{:02x}'.format((uuid.getnode() >> i) & 0xff) for i in range(0, 8 * 6, 8)][::-1])
+    cert.set_serial_number(12345)
+    cert.gmtime_adj_notBefore(0)
+    cert.gmtime_adj_notAfter(10*365*24*60*60)
+    cert.set_issuer(cert.get_subject())
+    cert.set_pubkey(k)
+    cert.sign(k, 'sha1')
+    #print cert.get_subject()
+    open(CERT_FILE, "wt").write(
+        crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
+    open(KEY_FILE, "wt").write(
+        crypto.dump_privatekey(crypto.FILETYPE_PEM, k))
+
+create_self_signed_cert()
