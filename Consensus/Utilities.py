2e5a67ba2adc628b569db72624b329b79f97cad4
Udhgee Murugesan <udhgee@gmail.com>
2017-11-03 16:22:26 -0400

    Added common ntp time

---
+++ Consensus/Utilities.py
@@ -0,0 +1,20 @@
+import time
+
+def gettime_ntp(addr='time.nist.gov'):
+    # http://code.activestate.com/recipes/117211-simple-very-sntp-client/
+    import socket
+    import struct
+    import sys
+    import time
+    TIME1970 = 2208988800L      # Thanks to F.Lundh
+    client = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+    data = '\x1b' + 47 * '\0'
+    client.sendto( data, (addr, 123))
+    data, address = client.recvfrom( 1024 )
+    if data:
+        t = struct.unpack( '!12I', data )[10]
+        t -= TIME1970
+        return time.ctime(t),t
+
+
+print gettime_ntp()
